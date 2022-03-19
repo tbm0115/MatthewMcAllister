b76a5e97be14fe3899a4a90520ba5dfa43617d4d
tbm0115 <tbm0115@gmail.com>
2022-03-18 17:44:24 -0700

    Added feeding chart

---
+++ MatthewMcAllister/Models/AgeHelper.cs
@@ -0,0 +1,37 @@
+﻿namespace MatthewMcAllister.Models
+{
+    public static class AgeHelper
+    {
+        public static DateTime Birthdate = new DateTime(2022, 03, 10, 7, 22, 0);
+
+        public static int GetAge(AgeType type = AgeType.Year)
+        {
+            DateTime now = DateTime.Now;
+            switch (type)
+            {
+                case AgeType.Year:
+                    return now.Year - Birthdate.Year;
+                    break;
+                case AgeType.Month:
+                    return (now.Month + now.Year * 12) - (Birthdate.Month + Birthdate.Year * 12);
+                    break;
+                case AgeType.Week:
+                    throw new NotImplementedException();
+                    break;
+                case AgeType.Day:
+                    throw new NotImplementedException();
+                    break;
+                default:
+                    throw new NotImplementedException();
+                    break;
+            }
+        }
+    }
+    public enum AgeType
+    {
+        Year,
+        Month,
+        Week,
+        Day
+    }
+}
---
+++ MatthewMcAllister/Models/FormulaFeedingRecommendation.cs
@@ -0,0 +1,14 @@
+﻿namespace MatthewMcAllister.Models
+{
+    public class FormulaFeedingRecommendation
+    {
+        public double MinimumMonths { get; set; }
+        public double MaximumMonths { get; set; }
+
+        public int MinimumFeedings { get; set; }
+        public int MaximumFeedings { get; set; }
+
+        public int MinimumOuncesPerFeeding { get; set; }
+        public int MaximumOuncesPerFeeding { get; set; }
+    }
+}
